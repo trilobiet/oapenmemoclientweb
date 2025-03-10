af7c9d735f2ca4342d150078be3a4c7547c4f3b2
acdhirr <acdhirr@trilobiet.nl>
2025-03-10 17:11:26 +0100

    Removed empty test

--- src/test/java/org/oapen/memoproject/clientweb/ClientwebApplicationTests.java
+++ src/test/java/org/oapen/memoproject/clientweb/ClientwebApplicationTests.java
@@ -1,12 +1,9 @@
 package org.oapen.memoproject.clientweb;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class ClientwebApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
